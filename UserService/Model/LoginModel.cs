64443a99d00490ae2f8ac365210db7273f7f15e9
Ravi Mandoriya <123547841+mandoriyaravi001@users.noreply.github.com>
2023-01-31 15:55:41 +0530

    Update LoginModel.cs

--- UserService/Model/LoginModel.cs
+++ UserService/Model/LoginModel.cs
@@ -10,7 +10,7 @@ namespace UserService.Model
     {
         [Required(ErrorMessage = "User Name is required")]
         public string Username { get; set; }
-
+//------------------
         [Required(ErrorMessage = "Password is required")]
         public string Password { get; set; }
     }
