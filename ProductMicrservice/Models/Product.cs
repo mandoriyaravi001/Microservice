b2c7ae573058fbe998b4d00ef550e1dd1faa0c19
Ravi Mandoriya <123547841+mandoriyaravi001@users.noreply.github.com>
2023-02-06 23:07:40 +0530

    Update Product.cs

--- ProductMicrservice/Models/Product.cs
+++ ProductMicrservice/Models/Product.cs
@@ -13,6 +13,7 @@ namespace ProductMicroservice.Models
         //-----
         //------
         //---------------------
+        //----------
        
         public string Description { get; set; }
         public decimal Price { get; set; }
